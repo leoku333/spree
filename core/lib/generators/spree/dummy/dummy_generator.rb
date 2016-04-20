641f6c0b07ed9c3509f622ad6e4a3114904648b7
Leo Ku <leoku333@gmail.com>
2016-04-20 13:23:01 -0500

    Update dummy_generator.rb

--- core/lib/generators/spree/dummy/dummy_generator.rb
+++ core/lib/generators/spree/dummy/dummy_generator.rb
@@ -27,7 +27,7 @@ module Spree
       # calling slice on a Thor::CoreExtensions::HashWithIndifferentAccess
       # object has been known to return nil
       opts = {}.merge(options).slice(*PASSTHROUGH_OPTIONS)
-      opts[:database] = 'sqlite3' if opts[:database].blank?
+      opts[:database] = 'postgres' if opts[:database].blank?
       opts[:force] = true
       opts[:skip_bundle] = true
       opts[:old_style_hash] = true
