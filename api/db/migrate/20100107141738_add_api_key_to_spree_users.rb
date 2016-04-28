d4d28119a5372c2ae599c1046da9fdeea206f672
Leo Ku <leoku333@gmail.com>
2016-04-28 15:28:18 -0500

    spree_api_key column is missing from users table

--- api/db/migrate/20100107141738_add_api_key_to_spree_users.rb
+++ api/db/migrate/20100107141738_add_api_key_to_spree_users.rb
@@ -1,7 +1,5 @@
 class AddApiKeyToSpreeUsers < ActiveRecord::Migration
   def change
-    unless defined?(User)
-      add_column :users, :spree_api_key, :string, :limit => 48
-    end
+    add_column :users, :spree_api_key, :string, :limit => 48
   end
 end
